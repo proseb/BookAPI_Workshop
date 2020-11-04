164adcc2175254c7a1a4cb182cfc2cf9322bf2e8
proseb <orliks2290@gmail.com>
2020-11-04 11:57:15 +0100

    Preparation before Hibernate use - completed

--- src/main/java/pl/coderslab/model/Book.java
+++ src/main/java/pl/coderslab/model/Book.java
@@ -1,7 +1,12 @@
 package pl.coderslab.model;
 
+import javax.persistence.*;
 
+@Entity
+@Table(name = "books")
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String isbn;
     private String title;
